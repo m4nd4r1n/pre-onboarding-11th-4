bfb48ca70b7ff3ceb763f1e590bd82479c087c00
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-07-19 21:44:21 +0900

    feat: Title 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Layout from './components/Layout';
 import Search from './components/Search';
+import Title from './components/Title';
 
 const App = () => {
   return (
     <Layout>
+      <Title />
       <Search />
     </Layout>
   );
